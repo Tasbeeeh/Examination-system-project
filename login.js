fe1402ce7e82cca9ad6cf549052fc2b8906a74b0
Tasbeeeh <tasbeehmohamed540@gmail.com>
2025-10-28 19:09:00 +0300

    Add login js code

---
+++ login.js
@@ -0,0 +1,61 @@
+function handleLogin(event) {
+    event.preventDefault(); 
+    console.log("login called");
+    clearErrors();
+
+    if (validateForm()) {
+        login();
+    }
+}
+
+function validateForm() {
+    let inputEmail = document.getElementById("inputEmail").value.trim();
+    let inputPassword = document.getElementById("inputPassword").value.trim();
+    let isValid = true;
+
+    if (inputEmail === "") {
+        document.getElementById("emailError").textContent = "Please enter your email";
+        isValid = false;
+    } 
+    else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputEmail)) {
+        document.getElementById("emailError").textContent = "Email format is incorrect";
+        isValid = false;
+    }
+
+    if (inputPassword === "") {
+        document.getElementById("passwordError").textContent = "Please enter your password";
+        isValid = false;
+    }
+
+    return isValid;
+}
+
+function clearErrors() {
+    document.getElementById("emailError").textContent = "";
+    document.getElementById("passwordError").textContent = "";
+}
+
+function login() {
+    let inputEmail = document.getElementById("inputEmail").value;
+    let inputPassword = document.getElementById("inputPassword").value;
+
+    let savedEmail = localStorage.getItem("email");
+    let savedPassword = localStorage.getItem("password");
+
+    if (inputEmail === savedEmail && inputPassword === savedPassword) {
+        window.location.href = "Exam.html";
+    } else {
+        document.getElementById("passwordError").textContent = "Invalid email or password";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("inputEmail").addEventListener("input", function() {
+        document.getElementById("emailError").textContent = "";
+    });
+    document.getElementById("inputPassword").addEventListener("input", function() {
+        document.getElementById("passwordError").textContent = "";
+    });
+});
+
+
